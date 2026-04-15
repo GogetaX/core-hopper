e454ef8b220d089361d2108a08fcd48a77ae4ed8
GogetaX <51264046+GogetaX@users.noreply.github.com>
2026-04-15 20:45:31 +0300

    123

--- scenes/screens/dig/next_boss_in_helper.gd
+++ scenes/screens/dig/next_boss_in_helper.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 func SyncData():
 	var glob_depth = GlobalSave.save_data.progress.global_depth
 	var next_boss_depth = GlobalBossDb.GetNextBossDepth(glob_depth)
-	get_parent().status_text = "BOSS BLOCK IN "+Global.CurrencyToString(next_boss_depth-glob_depth)+" M"
+	get_parent().status_text = "NEXT BOSS IN "+Global.CurrencyToString(next_boss_depth-glob_depth)+"m"
