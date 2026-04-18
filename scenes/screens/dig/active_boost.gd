281c5ac5d5e42b26d9a42e3e23223e0f7dd5203e
GogetaX <51264046+GogetaX@users.noreply.github.com>
2026-04-19 01:24:57 +0300

    123

--- scenes/screens/dig/active_boost.gd
+++ scenes/screens/dig/active_boost.gd
@@ -17,6 +17,10 @@ func SyncBoost():
 		var cur_boost = activated_boost_id[0]
 		cur_boost_id = cur_boost
 		var active_boost = GlobalTimedBonus.GetActivatedBoosterData(cur_boost)
+		if active_boost.effect_value == 1.0:
+			$CurActiveBoost/VList/boost_value.visible = false
+		else:
+			$CurActiveBoost/VList/boost_value.visible = true
 		$CurActiveBoost/VList/HBoxContainer/boost_name.text = active_boost.title
 		$CurActiveBoost/VList/boost_value.text = "x"+str(active_boost.effect_value).pad_decimals(1)
 		$CurActiveBoost/VList/HBoxContainer/boost_time_left.text = Global.SecondsToPrettyTimeString(active_boost.remaining_sec)
